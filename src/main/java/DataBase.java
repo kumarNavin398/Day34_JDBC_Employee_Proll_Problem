5251668ea6abca493fe7e85af3cb3ec586756663
KumarNavin <navins398@gmail.com>
2023-02-14 10:44:33 +0530

    ReteriveData

--- src/main/java/DataBase.java
+++ src/main/java/DataBase.java
@@ -2,18 +2,26 @@ import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.util.Enumeration;
+import java.sql.*;
 
 public class DataBase {
-    public static void main(String[] args) {
+    static Connection a = null;
+
+    public static void main(String[] args) throws SQLException {
+        a = connected();
+        reteriveData(a);
+    }
+
+
+    public static Connection connected() {
         String jdbcUrl = "jdbc:mysql://127.0.0.1:3306/payrollService1?useSSL=false";
         String userName = "root";
         String password = "Krunali29!";
-        Connection connection;
+        Connection connection = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
             System.out.println("Driver loaded");
-        } catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
         listDrivers();
@@ -24,6 +32,23 @@ public class DataBase {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return connection;
+    }
+
+    public static String reteriveData(Connection connection) throws SQLException {
+        String deptName = null;
+        String query = "select * from employeePayroll where id=? or name =?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1, 6);
+        preparedStatement.setString(2, "Priya");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            //      System.out.println(resultSet.getInt("id"));
+            //      System.out.println(resultSet.getString("name"));
+            deptName = (resultSet.getString("dept"));
+            //      System.out.println(resultSet.getString("startDate"));
+        }
+        return deptName;
     }
 
     public static void listDrivers() {
